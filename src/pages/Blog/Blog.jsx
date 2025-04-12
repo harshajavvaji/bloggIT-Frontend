47917e4f7431d5ec892257a427a81bc1a0ceea64
harshajavvaji <102543049+harshajavvaji@users.noreply.github.com>
2025-04-13 01:35:43 +0530

    solved  build error

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import BlogComponent from '../../components/BlogComponent/BlogComponent'
 import BlogsNavbar from '../../components/Navbar/Navbar'
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react';
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Form, Button } from 'react-bootstrap'
